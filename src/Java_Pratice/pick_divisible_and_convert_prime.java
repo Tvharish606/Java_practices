4916fcb80cf8ba5fb1ee846b22b53ef755616ed5
Harish <tvharish606@gmail.com>
2024-07-09 11:19:59 +0530

    "added_one_class"

--- src/Java_Pratice/pick_divisible_and_convert_prime.java
+++ src/Java_Pratice/pick_divisible_and_convert_prime.java
@@ -1,6 +1,30 @@
 package Java_Pratice;
 
-public class pick_divisible_and_convert_prime
-{
-    public
+public class pick_divisible_and_convert_prime {
+    public static void main(String[] args) {
+        int number = 81;
+        int div_count = 0;
+
+        for (int i = 1; i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                int prime_check_count = 0;
+                for (int k = 1; k <= i; k++)
+                {
+                    if (i % k == 0)
+                    {
+                        prime_check_count++;
+
+                    }
+                }
+                if(prime_check_count==2)
+                {
+                    System.out.println(i+ "is prime number");
+                }
+
+            }
+        }
+
+    }
 }
